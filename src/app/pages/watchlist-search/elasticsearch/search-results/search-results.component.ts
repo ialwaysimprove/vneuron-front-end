47c8454ef850f8ff0a317c90623c2adc40f6afc4
ialwaysimprove <ialwaysimprove@gmail.com>
2021-06-28 09:20:19 +0100

    Add buttons for CRUD Operations

--- src/app/pages/watchlist-search/elasticsearch/search-results/search-results.component.ts
+++ src/app/pages/watchlist-search/elasticsearch/search-results/search-results.component.ts
@@ -44,8 +44,24 @@ export class SearchResultsComponent implements OnInit {
   }
 
   settings = {
-    actions: false,
-    hideSubHeader: true,
+    // actions: false,
+    // hideSubHeader: true,
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+
+
     columns: {
       entity_id: {
         title: 'ID',
